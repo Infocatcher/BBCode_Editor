d3ec3880da1f62d85f7e913ef6ec647cfd80c3fa
Infocatcher <infocatcher.dev.null@gmail.com>
2013-01-24 21:20:44 +0400

    Fix undo/redo (only Google Chrome for now) (989ccf4ad22ca74f6501980e364f91f397ae594b)

--- editor.js
+++ editor.js
@@ -132,6 +132,22 @@ var bbCode = {
 		var ta = this._getTa();
 		ta.focus();
 		var onlyTagsShift = this.onlyTagsMask.test(text) ? RegExp["$" + this.onlyTagsCloseTagNum].length : 0;
+		try { // Google Chrome
+			if(document.queryCommandEnabled("insertText")) {
+				var ss = ta.selectionStart;
+				var se = ss + text.length - onlyTagsShift;
+				var v = ta.value;
+				document.execCommand("insertText", false, text);
+				if(ta.value == v)
+					throw "not changed";
+				ta.selectionStart = this.getSelectInserted() && !onlyTagsShift ? ss : se;
+				ta.selectionEnd = se;
+				return;
+			}
+		}
+		catch(e) {
+		}
+
 		if(typeof ta.selectionStart == "number") {
 			var sTop = ta.scrollTop;
 			var sHeight = ta.scrollHeight;
