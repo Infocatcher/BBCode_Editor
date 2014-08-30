Infocatcher/BBCode_Editor

commit cc33c3afc5b614573d958596a5ebac7587599956

Use name of textarea in backup key, if id is missing (#1)

editor.js
@@ -334,7 +334,7 @@ Editor.prototype = {
 		}
 		if(!this.storage)
 			return;
-		this.backupKey = "editor:" + this.ta.id + ":backup";
+		this.backupKey = "editor:" + this.ta.id || this.ta.name + ":backup";
 		this.backupTimeKey = this.backupKey + ":time";
 		var _this = this;
 		if(this.backupInterval > 0) {

commit ac9d035146437a88e1aa350f123b0e1c21fa46f4

Correctly generate key for localStorage (regression in cc33c3afc5b614573d958596a5ebac7587599956 commit) (#1)

editor.js
@@ -334,7 +334,7 @@ Editor.prototype = {
 		}
 		if(!this.storage)
 			return;
-		this.backupKey = "editor:" + this.ta.id || this.ta.name + ":backup";
+		this.backupKey = "editor:" + (this.ta.id || this.ta.name) + ":backup";
 		this.backupTimeKey = this.backupKey + ":time";
 		var _this = this;
 		if(this.backupInterval > 0) {